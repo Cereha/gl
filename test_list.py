b8a7989577b2b78d7b9761f90d016893400828cb
cereha <koroban.olya@gmail.com>
2021-02-19 21:15:04 +0200

    corect name test and funktion

--- test_list.py
+++ test_list.py
@@ -1,6 +1,6 @@
 import unittest
 
-def funk(a):
+def get_second_to_fifth_elements(a):
     return a[2:5]
 
 class TestListMethods(unittest.TestCase):
@@ -13,9 +13,9 @@ class TestListMethods(unittest.TestCase):
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
 
-    def test_cut_out_element(self):
+    def test_Olya_Koroban_FI94(self):
         a=[1,2,3,4,5,6,7,8,9]
-        self.assertEqual(funk(a),[3,4,5])
+        self.assertEqual(get_second_to_fifth_elements(a),[3,4,5])
 
 
 if __name__ == '__main__':
