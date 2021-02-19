b7170c66340a943b5cae5ae7f620192965627e42
cereha <koroban.olya@gmail.com>
2021-02-19 17:05:41 +0200

    add test

--- test_list.py
+++ test_list.py
@@ -1,15 +1,20 @@
 import unittest
-print("Hello")
+
 
 class TestListMethods(unittest.TestCase):
 
     def test_roman_tkalenko_fi_13(self):
-        self.assertEqual(len([]), 0)
+        self.assertEqual(len(['b']), 0)
         self.assertEqual(len(['a']), 1)
         self.assertEqual(len(['a', 'b']), 2)
 
     def test_roman_tkalenko_2(self):
         self.assertEqual(2, 2)
+
+    def test_cut_out_element(self):
+        a=['123456789']
+        self.assertEqual(funk(a),'2345')
+
 
 if __name__ == '__main__':
     unittest.main()
