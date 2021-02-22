0bec507cceb191011d05dc095d5bf059b9baa3e5
Koroban Olya <79316653+Cereha@users.noreply.github.com>
2021-02-22 20:20:49 +0200

    Update test_list.py

--- test_list.py
+++ test_list.py
@@ -1,7 +1,9 @@
 import unittest
 
+
 def get_second_to_fifth_elements(a):
     return a[2:5]
+
 
 def delete_element(list1):
     list1.remove(list1[-4])
@@ -106,11 +108,11 @@ class TestListMethods(unittest.TestCase):
     def test_Yelizaveta_Godovikova_FI94(self):
         list = ['a', 'k', 'j', 'y', 'r', 'o']
         self.assertEqual(del_third_element(list), ['a', 'k', 'y', 'r', 'o'])
-        
+
     def test_Olya_Koroban_FI94(self):
-        a=[1, 2, 3, 4, 5, 6, 7, 8, 9]
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(get_second_to_fifth_elements(a), [3, 4, 5])
-
-
+        
+        
 if __name__ == '__main__':
     unittest.main()
